0c0ea4d34a662aec5c47c384eeed5be320939c20
Jose R. Gonzalez <josegonzalez89@gmail.com>
2021-07-27 09:19:49 -0500

    add library path to version context
    Signed-off-by: Jose R. Gonzalez <josegonzalez89@gmail.com>

--- version/version.go
+++ version/version.go
@@ -1,18 +1,21 @@
+// Package version contains all identifiable versioning info for
+// describing the preflight project.
 package version
 
 import "fmt"
 
-// TODO restructure this to be local only these packages
-// TODO dynamically generate these at release
+var projectName = "github.com/redhat-openshift-ecosystem/openshift-preflight"
 var version = "unknown"
 var commit = "unknown"
 
 var Version = VersionContext{
+	Name:    projectName,
 	Version: version,
 	Commit:  commit,
 }
 
 type VersionContext struct {
+	Name    string `json:"name"`
 	Version string `json:"version"`
 	Commit  string `json:"commit"`
 }
