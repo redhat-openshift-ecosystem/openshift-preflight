002402b39e1c425649482d6b43c180be07372013
Samira Barouti <samira.barouti@gmail.com>
2021-09-07 09:45:48 -0400

    Add unit tests for deployableByOLM check + refactoring

--- certification/internal/policy/operator/operator_suite_test.go
+++ certification/internal/policy/operator/operator_suite_test.go
@@ -6,8 +6,13 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	operatorv1 "github.com/operator-framework/api/pkg/operators/v1"
+	operatorv1alpha1 "github.com/operator-framework/api/pkg/operators/v1alpha1"
 	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/internal/cli"
 	log "github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestOperator(t *testing.T) {
@@ -18,6 +23,9 @@ func TestOperator(t *testing.T) {
 func init() {
 	log.SetFormatter(&log.TextFormatter{})
 	log.SetLevel(log.TraceLevel)
+
+	viper.SetEnvPrefix("pflt")
+	viper.AutomaticEnv()
 }
 
 type FakeOperatorSdkEngine struct {
@@ -52,4 +60,249 @@ func (bose BadOperatorSdkEngine) BundleValidate(bundleImage string, opts cli.Ope
 		Outputs: []cli.OperatorSdkBundleValidateOutput{},
 	}
 	return &operatorSdkReport, errors.New("the Operator Sdk Bundle Validate has failed")
+}
+
+type FakeOpenshiftEngine struct{}
+
+func (foe FakeOpenshiftEngine) Setup() error {
+	return nil
+}
+
+func (foe FakeOpenshiftEngine) CreateNamespace(name string, opts cli.OpenshiftOptions) (*corev1.Namespace, error) {
+	return &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-ns",
+		},
+	}, nil
+}
+
+func (foe FakeOpenshiftEngine) DeleteNamespace(name string, opts cli.OpenshiftOptions) error {
+	return nil
+}
+
+func (foe FakeOpenshiftEngine) GetNamespace(name string) (*corev1.Namespace, error) {
+	return &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-ns",
+		},
+	}, nil
+}
+
+func (foe FakeOpenshiftEngine) CreateOperatorGroup(data cli.OperatorGroupData, opts cli.OpenshiftOptions) (*operatorv1.OperatorGroup, error) {
+	return &operatorv1.OperatorGroup{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-og",
+			Namespace: "test-ns",
+		},
+		Spec: operatorv1.OperatorGroupSpec{
+			TargetNamespaces: []string{"test-ns"},
+		},
+	}, nil
+}
+
+func (foe FakeOpenshiftEngine) DeleteOperatorGroup(name string, opts cli.OpenshiftOptions) error {
+	return nil
+}
+
+func (foe FakeOpenshiftEngine) GetOperatorGroup(name string, opts cli.OpenshiftOptions) (*operatorv1.OperatorGroup, error) {
+	return &operatorv1.OperatorGroup{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-og",
+			Namespace: "test-ns",
+		},
+		Spec: operatorv1.OperatorGroupSpec{
+			TargetNamespaces: []string{"test-ns"},
+		},
+	}, nil
+}
+
+func (foe FakeOpenshiftEngine) CreateCatalogSource(data cli.CatalogSourceData, opts cli.OpenshiftOptions) (*operatorv1alpha1.CatalogSource, error) {
+	return &operatorv1alpha1.CatalogSource{
+		Spec: operatorv1alpha1.CatalogSourceSpec{
+			SourceType: operatorv1alpha1.SourceTypeGrpc,
+			Image:      "indexImageUri",
+		},
+	}, nil
+}
+
+func (foe FakeOpenshiftEngine) DeleteCatalogSource(name string, opts cli.OpenshiftOptions) error {
+	return nil
+}
+
+func (foe FakeOpenshiftEngine) GetCatalogSource(name string, opts cli.OpenshiftOptions) (*operatorv1alpha1.CatalogSource, error) {
+	return &operatorv1alpha1.CatalogSource{
+		Spec: operatorv1alpha1.CatalogSourceSpec{
+			SourceType: operatorv1alpha1.SourceTypeGrpc,
+			Image:      "indexImageUri",
+		},
+	}, nil
+}
+
+func (foe FakeOpenshiftEngine) CreateSubscription(data cli.SubscriptionData, opts cli.OpenshiftOptions) (*operatorv1alpha1.Subscription, error) {
+	return &operatorv1alpha1.Subscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-sub",
+			Namespace: "test-ns",
+		},
+		Spec: &operatorv1alpha1.SubscriptionSpec{
+			CatalogSource:          "test-cs",
+			CatalogSourceNamespace: "openshift-marketplace",
+			Channel:                "stable",
+			Package:                "test-operator",
+		},
+		Status: operatorv1alpha1.SubscriptionStatus{
+			InstalledCSV: "csv-v0.0.0",
+		},
+	}, nil
+}
+
+func (foe FakeOpenshiftEngine) DeleteSubscription(name string, opts cli.OpenshiftOptions) error {
+	return nil
+}
+
+func (foe FakeOpenshiftEngine) GetSubscription(name string, opts cli.OpenshiftOptions) (*operatorv1alpha1.Subscription, error) {
+	return &operatorv1alpha1.Subscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-sub",
+			Namespace: "test-ns",
+		},
+		Spec: &operatorv1alpha1.SubscriptionSpec{
+			CatalogSource:          "test-cs",
+			CatalogSourceNamespace: "openshift-marketplace",
+			Channel:                "stable",
+			Package:                "test-operator",
+		},
+		Status: operatorv1alpha1.SubscriptionStatus{
+			InstalledCSV: "csv-v0.0.0",
+		},
+	}, nil
+}
+
+func (foe FakeOpenshiftEngine) GetCSV(name string, opts cli.OpenshiftOptions) (*operatorv1alpha1.ClusterServiceVersion, error) {
+	return &operatorv1alpha1.ClusterServiceVersion{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "csv-v0.0.0",
+			Namespace: "test-ns",
+		},
+		Spec: operatorv1alpha1.ClusterServiceVersionSpec{},
+		Status: operatorv1alpha1.ClusterServiceVersionStatus{
+			Phase: operatorv1alpha1.CSVPhaseSucceeded,
+		},
+	}, nil
+}
+
+type BadOpenshiftEngine struct{}
+
+func (foe BadOpenshiftEngine) Setup() error {
+	return nil
+}
+
+func (foe BadOpenshiftEngine) CreateNamespace(name string, opts cli.OpenshiftOptions) (*corev1.Namespace, error) {
+	return &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-ns",
+		},
+	}, nil
+}
+
+func (foe BadOpenshiftEngine) DeleteNamespace(name string, opts cli.OpenshiftOptions) error {
+	return nil
+}
+
+func (foe BadOpenshiftEngine) GetNamespace(name string) (*corev1.Namespace, error) {
+	return &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "test-ns",
+		},
+	}, nil
+}
+
+func (foe BadOpenshiftEngine) CreateOperatorGroup(data cli.OperatorGroupData, opts cli.OpenshiftOptions) (*operatorv1.OperatorGroup, error) {
+	return &operatorv1.OperatorGroup{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-og",
+			Namespace: "test-ns",
+		},
+		Spec: operatorv1.OperatorGroupSpec{
+			TargetNamespaces: []string{"test-ns"},
+		},
+	}, nil
+}
+
+func (foe BadOpenshiftEngine) DeleteOperatorGroup(name string, opts cli.OpenshiftOptions) error {
+	return nil
+}
+
+func (foe BadOpenshiftEngine) GetOperatorGroup(name string, opts cli.OpenshiftOptions) (*operatorv1.OperatorGroup, error) {
+	return &operatorv1.OperatorGroup{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-og",
+			Namespace: "test-ns",
+		},
+		Spec: operatorv1.OperatorGroupSpec{
+			TargetNamespaces: []string{"test-ns"},
+		},
+	}, nil
+}
+
+func (foe BadOpenshiftEngine) CreateCatalogSource(data cli.CatalogSourceData, opts cli.OpenshiftOptions) (*operatorv1alpha1.CatalogSource, error) {
+	return &operatorv1alpha1.CatalogSource{
+		Spec: operatorv1alpha1.CatalogSourceSpec{
+			SourceType: operatorv1alpha1.SourceTypeGrpc,
+			Image:      "indexImageUri",
+		},
+	}, nil
+}
+
+func (foe BadOpenshiftEngine) DeleteCatalogSource(name string, opts cli.OpenshiftOptions) error {
+	return nil
+}
+
+func (foe BadOpenshiftEngine) GetCatalogSource(name string, opts cli.OpenshiftOptions) (*operatorv1alpha1.CatalogSource, error) {
+	return &operatorv1alpha1.CatalogSource{
+		Spec: operatorv1alpha1.CatalogSourceSpec{
+			SourceType: operatorv1alpha1.SourceTypeGrpc,
+			Image:      "indexImageUri",
+		},
+	}, nil
+}
+
+func (foe BadOpenshiftEngine) CreateSubscription(data cli.SubscriptionData, opts cli.OpenshiftOptions) (*operatorv1alpha1.Subscription, error) {
+	return &operatorv1alpha1.Subscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-sub",
+			Namespace: "test-ns",
+		},
+		Spec: &operatorv1alpha1.SubscriptionSpec{
+			CatalogSource:          "test-cs",
+			CatalogSourceNamespace: "openshift-marketplace",
+			Channel:                "stable",
+			Package:                "test-operator",
+		},
+		Status: operatorv1alpha1.SubscriptionStatus{},
+	}, nil
+}
+
+func (foe BadOpenshiftEngine) DeleteSubscription(name string, opts cli.OpenshiftOptions) error {
+	return nil
+}
+
+func (foe BadOpenshiftEngine) GetSubscription(name string, opts cli.OpenshiftOptions) (*operatorv1alpha1.Subscription, error) {
+	return &operatorv1alpha1.Subscription{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-sub",
+			Namespace: "test-ns",
+		},
+		Spec: &operatorv1alpha1.SubscriptionSpec{
+			CatalogSource:          "test-cs",
+			CatalogSourceNamespace: "openshift-marketplace",
+			Channel:                "stable",
+			Package:                "test-operator",
+		},
+		Status: operatorv1alpha1.SubscriptionStatus{},
+	}, nil
+}
+
+func (foe BadOpenshiftEngine) GetCSV(name string, opts cli.OpenshiftOptions) (*operatorv1alpha1.ClusterServiceVersion, error) {
+	return nil, nil
 }
