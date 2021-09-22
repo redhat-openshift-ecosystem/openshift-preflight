c2400100c109ef0aacfc219a38a672d939f252cc
Brad P. Crochet <brad@redhat.com>
2021-09-22 12:18:35 -0400

    Reintroduce the JUnit XML formatter
    A Viper boolean is introduced merely called "junit". If it is true,
    it will write the results to the artifacts dir called results-junit.xml.
    
    Set PFLT_JUNIT envvar is set to 't', 'true', 1 or set "junit": true in
    config file.
    
    Fixes #268
    
    Signed-off-by: Brad P. Crochet <brad@redhat.com>

--- cmd/check.go
+++ cmd/check.go
@@ -1,7 +1,12 @@
 package cmd
 
 import (
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/artifacts"
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/formatters"
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/runtime"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var checkCmd = &cobra.Command{
@@ -12,4 +17,30 @@ var checkCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(checkCmd)
+}
+
+func writeJunitIfEnabled(results runtime.Results) error {
+	if !viper.GetBool("junit") {
+		return nil
+	}
+
+	var cfg runtime.Config
+	cfg.ResponseFormat = "junitxml"
+
+	junitformatter, err := formatters.NewForConfig(cfg)
+	if err != nil {
+		return err
+	}
+	junitResults, err := junitformatter.Format(results)
+	if err != nil {
+		return err
+	}
+
+	junitFilename, err := artifacts.WriteFile("results-junit.xml", string(junitResults))
+	if err != nil {
+		return err
+	}
+	log.Tracef("JUnitXML written to %s", junitFilename)
+
+	return nil
 }
