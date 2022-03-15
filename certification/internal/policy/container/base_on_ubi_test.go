0aaf872b6e7972da9ce1f3bb0e0e543e15aa49c5
Sid Kattoju <skattoju@redhat.com>
2022-03-15 09:00:05 -0400

    check image under test for a certified uncompressed top layer id

--- certification/internal/policy/container/base_on_ubi_test.go
+++ certification/internal/policy/container/base_on_ubi_test.go
@@ -1,56 +1,36 @@
 package container
 
 import (
+	"context"
 	"os"
-	"path/filepath"
 
 	cranev1 "github.com/google/go-containerregistry/pkg/v1"
 	fakecranev1 "github.com/google/go-containerregistry/pkg/v1/fake"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification"
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/pyxis"
 )
 
-func labelsForUbiCheck(label string, bad bool) map[string]string {
-	labels := map[string]string{
-		"name":                 "name",
-		"vendor":               "vendor",
-		"version":              "version",
-		"release":              "release",
-		"summary":              "summary",
-		"description":          "description",
-		"com.redhat.component": label,
-	}
-
-	if bad {
-		delete(labels, "com.redhat.component")
-	}
-
-	return labels
-}
-
-func goodLabelsConfigFile() (*cranev1.ConfigFile, error) {
+func ConfigFile() (*cranev1.ConfigFile, error) {
 	return &cranev1.ConfigFile{
-		Config: cranev1.Config{
-			Labels: labelsForUbiCheck("ubi8-container", false),
-		},
+		Config: cranev1.Config{},
 	}, nil
 }
 
-func missingUbiLableConfigFile() (*cranev1.ConfigFile, error) {
-	return &cranev1.ConfigFile{
-		Config: cranev1.Config{
-			Labels: labelsForUbiCheck("", true),
-		},
-	}, nil
+type fakeLayerHashChecker struct{}
+
+func (flhc *fakeLayerHashChecker) CheckRedHatLayers(ctx context.Context, layers []cranev1.Hash) ([]pyxis.CertImage, error) {
+	var matchingImages []pyxis.CertImage
+	matchingImages = append(matchingImages, pyxis.CertImage{})
+	return matchingImages, nil
 }
 
-func badLabelsConfigFile() (*cranev1.ConfigFile, error) {
-	return &cranev1.ConfigFile{
-		Config: cranev1.Config{
-			Labels: labelsForUbiCheck("doesnothavestring", false),
-		},
-	}, nil
+type fakeLayerHashCheckerNoMatch struct{}
+
+func (flhc *fakeLayerHashCheckerNoMatch) CheckRedHatLayers(ctx context.Context, layers []cranev1.Hash) ([]pyxis.CertImage, error) {
+	var matchingImages []pyxis.CertImage
+	return matchingImages, nil
 }
 
 var _ = Describe("BaseOnUBI", func() {
@@ -59,45 +39,21 @@ var _ = Describe("BaseOnUBI", func() {
 		imageRef        certification.ImageReference
 	)
 
-	const osrelease = "os-release"
-
 	BeforeEach(func() {
 		fakeImage := fakecranev1.FakeImage{
-			ConfigFileStub: goodLabelsConfigFile,
+			ConfigFileStub: ConfigFile,
 		}
 		imageRef.ImageInfo = &fakeImage
-		var err error
-		tmpDir, err := os.MkdirTemp("", "based-on-ubi-*")
-		Expect(err).ToNot(HaveOccurred())
-		err = os.Mkdir(filepath.Join(tmpDir, "etc"), 0o755)
-		Expect(err).ToNot(HaveOccurred())
-		err = os.WriteFile(filepath.Join(tmpDir, "etc", osrelease), []byte(`ID="rhel"
-NAME="Red Hat Enterprise Linux"
-`), 0o644)
-		Expect(err).ToNot(HaveOccurred())
-		imageRef.ImageFSPath = tmpDir
 	})
 	AfterEach(func() {
 		os.RemoveAll(imageRef.ImageFSPath)
 	})
 	Describe("Checking for UBI as a base", func() {
-		Context("When it is based on UBI 8", func() {
-			Context("and has the correct os-release", func() {
-				It("should pass Validate", func() {
-					ok, err := basedOnUbiCheck.Validate(imageRef)
-					Expect(err).ToNot(HaveOccurred())
-					Expect(ok).To(BeTrue())
-				})
+		Context("When the image contains a layer hash that is a ubi or ubi derived uncompressed top layer id", func() {
+			JustBeforeEach(func() {
+				basedOnUbiCheck.LayerHashCheckEngine = &fakeLayerHashChecker{}
 			})
-		})
-		Context("When it is based on UBI 7", func() {
-			Context("and has the correct os-release", func() {
-				JustBeforeEach(func() {
-					err := os.WriteFile(filepath.Join(imageRef.ImageFSPath, "etc", osrelease), []byte(`ID="rhel"
-NAME="Red Hat Enterprise Linux Server"
-`), 0o644)
-					Expect(err).ToNot(HaveOccurred())
-				})
+			Context("and pyxis returns a match", func() {
 				It("should pass Validate", func() {
 					ok, err := basedOnUbiCheck.Validate(imageRef)
 					Expect(err).ToNot(HaveOccurred())
@@ -106,25 +62,16 @@ NAME="Red Hat Enterprise Linux Server"
 			})
 		})
 		Context("When it is not based on UBI", func() {
-			Context("and has a bad os-release", func() {
-				JustBeforeEach(func() {
-					err := os.WriteFile(filepath.Join(imageRef.ImageFSPath, "etc", osrelease), []byte("Not a good file"), 0o644)
-					Expect(err).ToNot(HaveOccurred())
-				})
-				It("should not pass Validate", func() {
-					ok, err := basedOnUbiCheck.Validate(imageRef)
-					Expect(err).ToNot(HaveOccurred())
-					Expect(ok).To(BeFalse())
-				})
+			JustBeforeEach(func() {
+				basedOnUbiCheck.LayerHashCheckEngine = &fakeLayerHashCheckerNoMatch{}
 			})
-			Context("and os-release is missing", func() {
-				JustBeforeEach(func() {
-					os.Remove(filepath.Join(imageRef.ImageFSPath, "etc", osrelease))
-				})
-				It("should not pass Validate", func() {
-					ok, err := basedOnUbiCheck.Validate(imageRef)
-					Expect(err).To(HaveOccurred())
-					Expect(ok).To(BeFalse())
+			Context("When the image does not contain a layer hash that is a ubi or ubi derived uncompressed top layer id", func() {
+				Context("and pyxis returns no matches", func() {
+					It("should not pass Validate", func() {
+						ok, err := basedOnUbiCheck.Validate(imageRef)
+						Expect(err).ToNot(HaveOccurred())
+						Expect(ok).To(BeFalse())
+					})
 				})
 			})
 		})
