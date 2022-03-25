333392b53e03550a59eb3085247f430e40bf56e5
Brad P. Crochet <brad@redhat.com>
2022-03-25 15:47:48 -0400

    Use the default pyxis host for BasedOnUBI (#520)
    Since the default pyxis host is prod, that is what we should pass
    to the BasedOnUBI pyxis engine. It removes the "magic constant" from
    the initialization.
    
    Signed-off-by: Brad P. Crochet <brad@redhat.com>

--- certification/defaults.go
+++ certification/defaults.go
@@ -4,5 +4,6 @@ var (
 	DefaultCertImageFilename   = "cert-image.json"
 	DefaultRPMManifestFilename = "rpm-manifest.json"
 	DefaultTestResultsFilename = "results.json"
+	DefaultPyxisHost           = "catalog.redhat.com/api/containers"
 	SystemdDir                 = "/etc/systemd/system"
 )
