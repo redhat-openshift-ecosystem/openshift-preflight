1524cd598d8ce860e8de66771624eafe1e7d541a
Brad P. Crochet <brad@redhat.com>
2022-04-14 10:59:52 -0400

    Prefer the usage of pyxis env instead of pyxis host
    Create a map, and use that map to lookup what the pyxis host and
    path should be, rather than forcing the user to know that information.
    
    If pyxis host is set, it will override the env. It is assumed that the
    user knows what they are doing.
    
    Signed-off-by: Brad P. Crochet <brad@redhat.com>

--- certification/defaults.go
+++ certification/defaults.go
@@ -5,5 +5,6 @@ var (
 	DefaultRPMManifestFilename = "rpm-manifest.json"
 	DefaultTestResultsFilename = "results.json"
 	DefaultPyxisHost           = "catalog.redhat.com/api/containers"
+	DefaultPyxisEnv            = "prod"
 	SystemdDir                 = "/etc/systemd/system"
 )
