4ff13cc5a68ee22fffde738c8537f5b19a49dd6f
Rose Crisp <rocrisp@redhat.com>
2022-05-25 08:56:40 -0400

    Update container certification doc url #648

--- certification/internal/policy/container/defaults.go
+++ certification/internal/policy/container/defaults.go
@@ -7,5 +7,5 @@ import (
 var (
 	checkContainerTimeout time.Duration = 10 * time.Second
 	waitContainer         time.Duration = 2 * time.Second
-	certDocumentationURL                = "https://access.redhat.com/documentation/en-us/red_hat_openshift_certification/4.9/html-single/red_hat_openshift_software_certification_policy_guide/index#assembly-requirements-for-container-images_openshift-sw-cert-policy-introduction"
+	certDocumentationURL                = "https://access.redhat.com/documentation/en-us/red_hat_software_certification/8.45/html/red_hat_openshift_software_certification_policy_guide/assembly-requirements-for-container-images_openshift-sw-cert-policy-introduction"
 )
