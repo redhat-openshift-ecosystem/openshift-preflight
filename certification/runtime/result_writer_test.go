20fdf44096d563d9275ffd17b4cd9742ca5578ee
Jose R. Gonzalez <jose@flutes.dev>
2022-06-23 13:27:13 -0500

    add new ifaces for cmd execution with implementations
    Signed-off-by: Jose R. Gonzalez <jose@flutes.dev>

---
+++ certification/runtime/result_writer_test.go
@@ -0,0 +1,52 @@
+package runtime
+
+import (
+	"os"
+	"path"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Result Writers", func() {
+	var resultWriterTestDir string
+
+	BeforeEach(func() {
+		// instantiate err to make sure we can equal-assign in the following line.
+		var err error
+		resultWriterTestDir, err = os.MkdirTemp(os.TempDir(), "rw-test-*")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(len(resultWriterTestDir)).ToNot(BeZero())
+	})
+
+	AfterEach(func() {
+		err := os.RemoveAll(resultWriterTestDir)
+		Expect(err).ToNot(HaveOccurred())
+	})
+
+	Context("File-based Result Writer", func() {
+		var rw *ResultWriterFile
+
+		BeforeEach(func() {
+			rw = &ResultWriterFile{}
+		})
+
+		Context("When using the file-based result writer", func() {
+			It("should open, be writeable, and close successfully", func() {
+				p := path.Join(resultWriterTestDir, "foo.txt")
+				f, err := rw.OpenFile(p)
+				defer f.Close()
+				Expect(err).ToNot(HaveOccurred())
+				Expect(p).To(BeAnExistingFile())
+
+				toWrite := []byte("testing")
+				_, err = f.Write(toWrite)
+				Expect(err).ToNot(HaveOccurred())
+
+				contents, err := os.ReadFile(p)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(contents).To(Equal(toWrite))
+			})
+		})
+	})
+})
---
+++ cmd/types.go
@@ -0,0 +1,176 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"time"
+
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification"
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/artifacts"
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/pyxis"
+	log "github.com/sirupsen/logrus"
+)
+
+// resultWriter defines methods associated with writing check results.
+type resultWriter interface {
+	OpenFile(name string) (io.WriteCloser, error)
+	io.WriteCloser
+}
+
+// resultSubmitter defines methods associated with submitting results to Red HAt.
+type resultSubmitter interface {
+	Submit(context.Context) error
+}
+
+// pyxisClient defines pyxis API interactions that are relevant to check executions in cmd.
+type pyxisClient interface {
+	FindImagesByDigest(ctx context.Context, digests []string) ([]pyxis.CertImage, error)
+	GetProject(context.Context) (*pyxis.CertProject, error)
+	SubmitResults(context.Context, *pyxis.CertificationInput) (*pyxis.CertificationResults, error)
+}
+
+// newPyxisClient initializes a pyxisClient with relevant information from cfg.
+// If the the CertificationProjectID, PyxisAPIToken, or PyxisHost are empty, then nil is returned.
+// Callers should treat a nil pyxis client as an indicator that pyxis calls should not be made.
+func newPyxisClient(ctx context.Context, cfg certification.Config) pyxisClient {
+	if cfg.CertificationProjectID() == "" || cfg.PyxisAPIToken() == "" || cfg.PyxisHost() == "" {
+		return nil
+	}
+
+	return pyxis.NewPyxisClient(
+		cfg.PyxisHost(),
+		cfg.PyxisAPIToken(),
+		cfg.CertificationProjectID(),
+		&http.Client{Timeout: 60 * time.Second},
+	)
+}
+
+// containerCertificationSubmitter submits container results to Pyxis, and implements
+// a resultSubmitter.
+type containerCertificationSubmitter struct {
+	certificationProjectID string
+	pyxis                  pyxisClient
+	dockerConfig           string
+	preflightLogFile       string
+}
+
+func (s *containerCertificationSubmitter) Submit(ctx context.Context) error {
+	log.Info("preparing results that will be submitted to Red Hat")
+
+	// get the project info from pyxis
+	certProject, err := s.pyxis.GetProject(ctx)
+	if err != nil {
+		return fmt.Errorf("could not retrieve project: %w", err)
+	}
+
+	// Ensure that a certProject was returned. In theory we would expect pyxis
+	// to throw an error if no project is returned, but in the event that it doesn't
+	// we need to confirm before we proceed in order to prevent a runtime panic
+	// setting the DockerConfigJSON below.
+	if certProject == nil {
+		return fmt.Errorf("no certification project was returned from pyxis")
+	}
+
+	log.Tracef("CertProject: %+v", certProject)
+
+	// read the provided docker config
+	dockerConfigJsonBytes, err := os.ReadFile(s.dockerConfig)
+	if err != nil {
+		return err
+	}
+
+	certProject.Container.DockerConfigJSON = string(dockerConfigJsonBytes)
+
+	// prepare submission. We ignore the error because nil checks for the certProject
+	// are done earlier to prevent panics, and that's the only error case for this function.
+	submission, _ := pyxis.NewCertificationInput(certProject)
+
+	certImage, err := os.Open(path.Join(artifacts.Path(), certification.DefaultCertImageFilename))
+	defer certImage.Close()
+	if err != nil {
+		return fmt.Errorf("could not open file for submission: %s: %w",
+			certification.DefaultCertImageFilename,
+			err,
+		)
+	}
+	preflightResults, err := os.Open(path.Join(artifacts.Path(), certification.DefaultTestResultsFilename))
+	defer preflightResults.Close()
+	if err != nil {
+		return fmt.Errorf(
+			"could not open file for submission: %s: %w",
+			certification.DefaultTestResultsFilename,
+			err,
+		)
+	}
+	rpmManifest, err := os.Open(path.Join(artifacts.Path(), certification.DefaultRPMManifestFilename))
+	defer rpmManifest.Close()
+	if err != nil {
+		return fmt.Errorf(
+			"could not open file for submission: %s: %w",
+			certification.DefaultRPMManifestFilename,
+			err,
+		)
+	}
+	logfile, err := os.Open(s.preflightLogFile)
+	defer logfile.Close()
+	if err != nil {
+		return fmt.Errorf(
+			"could not open file for submission: %s: %w",
+			s.preflightLogFile,
+			err,
+		)
+	}
+	submission.
+		// The engine writes the certified image config to disk in a Pyxis-specific format.
+		WithCertImage(certImage).
+		// Include Preflight's test results in our submission. pyxis.TestResults embeds them.
+		WithPreflightResults(preflightResults).
+		// The certification engine writes the rpmManifest for images not based on scratch.
+		WithRPMManifest(rpmManifest).
+		// Include the preflight execution log file.
+		WithArtifact(logfile, filepath.Base(s.preflightLogFile))
+
+	input, err := submission.Finalize()
+	if err != nil {
+		return fmt.Errorf("unable to finalize data that would be sent to pyxis: %w", err)
+	}
+
+	certResults, err := s.pyxis.SubmitResults(ctx, input)
+	if err != nil {
+		return fmt.Errorf("could not submit to pyxis: %w", err)
+	}
+
+	log.Info("Test results have been submitted to Red Hat.")
+	log.Info("These results will be reviewed by Red Hat for final certification.")
+	log.Infof("The container's image id is: %s.", certResults.CertImage.ID)
+	log.Infof("Please check %s to view scan results.", buildScanResultsURL(s.certificationProjectID, certResults.CertImage.ID))
+	log.Infof("Please check %s to monitor the progress.", buildOverviewURL(s.certificationProjectID))
+
+	return nil
+}
+
+// noopSubmitter is a no-op resultSubmitter that optionally logs a message
+// and a reason as to why results were not submitted.
+type noopSubmitter struct {
+	emitLog bool
+	reason  string
+	log     *log.Logger
+}
+
+func (s *noopSubmitter) Submit(ctx context.Context) error {
+	if s.emitLog {
+		msg := "Results are not being sent for submission."
+		if s.reason != "" {
+			msg = fmt.Sprintf("%s Reason: %s.", msg, s.reason)
+		}
+
+		s.log.Info(msg)
+	}
+
+	return nil
+}
