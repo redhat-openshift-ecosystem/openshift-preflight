f28e41bea575bf29bc888167ddbf48094ef36f6b
Jose R. Gonzalez <jose@flutes.dev>
2022-06-23 13:27:13 -0500

    add new ifaces for cmd execution with implementations
    Signed-off-by: Jose R. Gonzalez <jose@flutes.dev>

---
+++ certification/runtime/result_writer.go
@@ -0,0 +1,33 @@
+package runtime
+
+import (
+	"io"
+	"os"
+)
+
+// ResultWriterFile implements a resultWriter for use at preflight runtime.
+type ResultWriterFile struct {
+	file *os.File
+}
+
+// OpenFile will open the expected file for writing.
+func (f *ResultWriterFile) OpenFile(name string) (io.WriteCloser, error) {
+	file, err := os.OpenFile(
+		name,
+		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
+		0o600)
+	if err != nil {
+		return nil, err
+	}
+
+	f.file = file // so we can close it later.
+	return f, nil
+}
+
+func (f *ResultWriterFile) Close() error {
+	return f.file.Close()
+}
+
+func (f *ResultWriterFile) Write(p []byte) (int, error) {
+	return f.file.Write(p)
+}
