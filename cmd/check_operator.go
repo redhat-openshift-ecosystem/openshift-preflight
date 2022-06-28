6189604f7e75b34c49234a15056c284382559247
Jose R. Gonzalez <jose@flutes.dev>
2022-06-28 11:41:40 -0500

    refactor check operator to leverage interface-based execution
    Signed-off-by: Jose R. Gonzalez <jose@flutes.dev>

--- cmd/check_operator.go
+++ cmd/check_operator.go
@@ -3,11 +3,8 @@ package cmd
 import (
 	"context"
 	"fmt"
-	"io"
 	"os"
-	"path/filepath"
 
-	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/artifacts"
 	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/engine"
 	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/formatters"
 	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/policy"
@@ -22,34 +19,70 @@ var checkOperatorCmd = &cobra.Command{
 	Use:   "operator",
 	Short: "Run checks for an Operator",
 	Long:  `This command will run the Certification checks for an Operator bundle image. `,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 {
-			return fmt.Errorf("an operator image positional argument is required")
-		}
-		return nil
-	},
+	Args:  checkOperatorPositionalArgs,
 	// this fmt.Sprintf is in place to keep spacing consistent with cobras two spaces that's used in: Usage, Flags, etc
 	Example: fmt.Sprintf("  %s", "preflight check operator quay.io/repo-name/operator-bundle:version"),
 	RunE:    checkOperatorRunE,
 }
 
-// checkOperatorRunE is a cobra RunE compatible function that prepares
-// the user configuration for check operator.
-func checkOperatorRunE(cmd *cobra.Command, args []string) error {
-	log.Info("certification library version ", version.Version.String())
+// checkOperatorRunner contains all of the components necessary to run checkOperator.
+type checkOperatorRunner struct {
+	cfg       *runtime.Config
+	eng       engine.CheckEngine
+	formatter formatters.ResponseFormatter
+	rw        resultWriter
+}
 
-	ctx := cmd.Context()
+// newCheckOperatorRunner returns a checkOperatorRunner containing all of the tooling necessary
+// to run checkOperator.
+func newCheckOperatorRunner(ctx context.Context, cfg *runtime.Config) (*checkOperatorRunner, error) {
+	cfg.Policy = policy.PolicyOperator
+	cfg.Submit = false // there's no such thing as submitting for operators today.
 
-	operatorImage := args[0]
+	engine, err := engine.NewForConfig(ctx, cfg.ReadOnly())
+	if err != nil {
+		return nil, err
+	}
 
+	fmttr, err := formatters.NewForConfig(cfg.ReadOnly())
+	if err != nil {
+		return nil, err
+	}
+
+	return &checkOperatorRunner{
+		cfg:       cfg,
+		eng:       engine,
+		formatter: fmttr,
+		rw:        &runtime.ResultWriterFile{},
+	}, nil
+}
+
+// ensureKubeconfigIsSet ensures that the KUBECONFIG environment variable has a value.
+func ensureKubeconfigIsSet() error {
 	if _, ok := os.LookupEnv("KUBECONFIG"); !ok {
 		return fmt.Errorf("environment variable KUBECONFIG could not be found")
 	}
 
+	return nil
+}
+
+// ensureIndexImageConfigIsSet ensures that the PFLT_INDEXIMAGE environment variable has
+// a value.
+func ensureIndexImageConfigIsSet() error {
 	if catalogImage := viper.GetString("indexImage"); len(catalogImage) == 0 {
 		return fmt.Errorf("environment variable PFLT_INDEXIMAGE could not be found")
 	}
 
+	return nil
+}
+
+// checkOperatorRunE executes checkOperator using the user args to inform the execution.
+func checkOperatorRunE(cmd *cobra.Command, args []string) error {
+	log.Info("certification library version ", version.Version.String())
+	ctx := cmd.Context()
+	operatorImage := args[0]
+
+	// Render the Viper configuration as a runtime.Config
 	cfg, err := runtime.NewConfigFrom(*viper.GetViper())
 	if err != nil {
 		return fmt.Errorf("invalid configuration: %w", err)
@@ -60,67 +93,35 @@ func checkOperatorRunE(cmd *cobra.Command, args []string) error {
 	cfg.Bundle = true
 	cfg.Scratch = true
 
+	checkOperator, err := newCheckOperatorRunner(ctx, cfg)
+	if err != nil {
+		return err
+	}
+
 	// Run the operator check
 	cmd.SilenceUsage = true
-	return checkOperator(ctx, cfg)
+	return preflightCheck(ctx,
+		checkOperator.cfg,
+		nil, // no pyxisClient is necessary
+		checkOperator.eng,
+		checkOperator.formatter,
+		checkOperator.rw,
+		&noopSubmitter{}, // we do not submit these results.
+	)
 }
 
-func checkOperator(ctx context.Context, cfg *runtime.Config) error {
-	cfg.Policy = policy.PolicyOperator
-
-	// configure the artifacts directory if the user requested a different directory.
-	if cfg.Artifacts != "" {
-		artifacts.SetDir(cfg.Artifacts)
+func checkOperatorPositionalArgs(cmd *cobra.Command, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("an operator bundle image positional argument is required")
 	}
 
-	engine, err := engine.NewForConfig(ctx, cfg.ReadOnly())
-	if err != nil {
+	if err := ensureKubeconfigIsSet(); err != nil {
 		return err
 	}
 
-	formatter, err := formatters.NewForConfig(cfg.ReadOnly())
-	if err != nil {
+	if err := ensureIndexImageConfigIsSet(); err != nil {
 		return err
 	}
-
-	// create the results file early to catch cases where we are not
-	// able to write to the filesystem before we attempt to execute checks.
-	resultsFile, err := os.OpenFile(
-		filepath.Join(artifacts.Path(), resultsFilenameWithExtension(formatter.FileExtension())),
-		os.O_WRONLY|os.O_CREATE|os.O_TRUNC,
-		0o600,
-	)
-	if err != nil {
-		return err
-	}
-
-	// also write to stdout
-	resultsOutputTarget := io.MultiWriter(os.Stdout, resultsFile)
-
-	// execute the checks
-	if err := engine.ExecuteChecks(ctx); err != nil {
-		return err
-	}
-	results := engine.Results(ctx)
-
-	// return results to the user and then close output files
-	formattedResults, err := formatter.Format(ctx, results)
-	if err != nil {
-		return err
-	}
-
-	fmt.Fprint(resultsOutputTarget, string(formattedResults))
-	if err := resultsFile.Close(); err != nil {
-		return err
-	}
-
-	if cfg.WriteJUnit {
-		if err := writeJUnit(ctx, results); err != nil {
-			return err
-		}
-	}
-
-	log.Infof("Preflight result: %s", convertPassedOverall(results.PassedOverall))
 
 	return nil
 }
---
+++ cmd/check_operator_test.go
@@ -0,0 +1,166 @@
+package cmd
+
+import (
+	"context"
+	"os"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/engine"
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/formatters"
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/runtime"
+	"github.com/spf13/viper"
+)
+
+var _ = Describe("Check Operator", func() {
+	Context("when running the check operator subcommand", func() {
+		Context("without the operator bundle image being provided", func() {
+			It("should return an error", func() {
+				_, err := executeCommand(rootCmd, "check", "operator")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		Context("without having set the KUBECONFIG environment variable", func() {
+			It("should return an error", func() {
+				out, err := executeCommand(rootCmd, "check", "operator", "quay.io/example/image:mytag")
+				Expect(err).To(HaveOccurred())
+				Expect(out).To(ContainSubstring("KUBECONFIG could not"))
+			})
+		})
+
+		Context("without having set the PFLT_INDEXIMAGE environment variable", func() {
+			BeforeEach(func() { os.Setenv("KUBECONFIG", "foo") })
+			AfterEach(func() { os.Unsetenv("KUBECONFIG") })
+			It("should return an error", func() {
+				out, err := executeCommand(rootCmd, "check", "operator", "quay.io/example/image:mytag")
+				Expect(err).To(HaveOccurred())
+				Expect(out).To(ContainSubstring("PFLT_INDEXIMAGE could not"))
+			})
+		})
+
+		Context("With all of the required parameters", func() {
+			BeforeEach(func() {
+				viper.Set("indexImage", "foo")
+				os.Setenv("KUBECONFIG", "foo")
+			})
+
+			AfterEach(func() {
+				viper.Set("indexImage", "")
+				os.Unsetenv("KUBECONFIG")
+			})
+
+			It("should reach the core logic, but throw an error because of the placeholder values", func() {
+				_, err := executeCommand(rootCmd, "check", "operator", "quay.io/example/image:mytag")
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Context("When checking for required environment variables", func() {
+		Context("specifically, KUBECONFIG", func() {
+			BeforeEach(func() { os.Setenv("KUBECONFIG", "foo") })
+			AfterEach(func() { os.Unsetenv("KUBECONFIG") })
+			It("should not encounter an error if the value is set", func() {
+				err := ensureKubeconfigIsSet()
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("should encounter an error if the value is not set", func() {
+				os.Unsetenv("KUBECONFIG")
+				err := ensureKubeconfigIsSet()
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		Context("specifically, PFLT_INDEXIMAGE", func() {
+			BeforeEach(func() { viper.Set("indexImage", "foo") })
+			AfterEach(func() { viper.Set("indexImage", "") })
+			It("should not encounter an error if the value is set", func() {
+				err := ensureIndexImageConfigIsSet()
+				Expect(err).ToNot(HaveOccurred())
+			})
+
+			It("should encounter an error if the value is not set", func() {
+				viper.Set("indexImage", "")
+				err := ensureIndexImageConfigIsSet()
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Context("When instantiating a checkOperatorRunner", func() {
+		var cfg *runtime.Config
+		BeforeEach(func() {
+			cfg = &runtime.Config{
+				Image:          "quay.io/example/foo:latest",
+				ResponseFormat: formatters.DefaultFormat,
+			}
+		})
+
+		Context("with a valid policy formatter", func() {
+			It("should return with no error, and the appropriate formatter", func() {
+				cfg.ResponseFormat = "xml"
+				runner, err := newCheckOperatorRunner(context.TODO(), cfg)
+				Expect(err).ToNot(HaveOccurred())
+				expectedFormatter, err := formatters.NewByName(cfg.ResponseFormat)
+				Expect(err).ToNot(HaveOccurred())
+				Expect(runner.formatter.PrettyName()).To(Equal(expectedFormatter.PrettyName()))
+			})
+		})
+
+		Context("with an invalid policy formatter", func() {
+			It("should return an error", func() {
+				cfg.ResponseFormat = "foo"
+				_, err := newCheckOperatorRunner(context.TODO(), cfg)
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		Context("with an invalid policy definition", func() {
+			It("should return the container policy engine anyway", func() {
+				runner, err := newCheckOperatorRunner(context.TODO(), cfg)
+				Expect(err).ToNot(HaveOccurred())
+
+				expectedEngine, err := engine.NewForConfig(context.TODO(), cfg.ReadOnly())
+				Expect(runner.eng).To(BeEquivalentTo(expectedEngine))
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("with an invalid formatter definition", func() {
+			It("should return an error", func() {
+				cfg.ResponseFormat = "foo"
+				_, err := newCheckOperatorRunner(context.TODO(), cfg)
+				Expect(err).To(HaveOccurred())
+			})
+		})
+
+		It("should contain a ResultWriterFile resultWriter", func() {
+			runner, err := newCheckOperatorRunner(context.TODO(), cfg)
+			Expect(err).ToNot(HaveOccurred())
+			_, rwIsExpectedType := runner.rw.(*runtime.ResultWriterFile)
+			Expect(rwIsExpectedType).To(BeTrue())
+		})
+	})
+
+	Context("When testing positional arg parsing", func() {
+		// failure cases are tested earlier in this file by running executeCommand.
+		// This tests the success case using the standalone function in order
+		// to prevent trying to run the entire RunE func in previous cases.
+		posArgs := []string{"firstparam"}
+		BeforeEach(func() {
+			viper.Set("indexImage", "foo")
+			os.Setenv("KUBECONFIG", "foo")
+		})
+
+		AfterEach(func() {
+			viper.Set("indexImage", "")
+			os.Unsetenv("KUBECONFIG")
+		})
+		It("should succeed when all positional arg constraints and environment constraints are correct", func() {
+			err := checkOperatorPositionalArgs(checkOperatorCmd, posArgs)
+			Expect(err).ToNot(HaveOccurred())
+		})
+	})
+})
