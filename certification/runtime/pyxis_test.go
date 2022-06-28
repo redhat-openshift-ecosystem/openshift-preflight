6042c73e5bb5f5fef7c8209b1447fa7aa927b021
Jose R. Gonzalez <jose@flutes.dev>
2022-06-28 14:54:34 -0500

    test the pyxis host lookup behavior
    Signed-off-by: Jose R. Gonzalez <jose@flutes.dev>

---
+++ certification/runtime/pyxis_test.go
@@ -0,0 +1,30 @@
+package runtime
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Pyxis Host Lookup", func() {
+	When("Resolving the pyxis host", func() {
+		Context("having no host override", func() {
+			It("should return the default value for the requested environment", func() {
+				val := pyxisHostLookup("prod", "")
+				Expect(val).To(Equal("catalog.redhat.com/api/containers"))
+			})
+			Context("having an invalid environment value", func() {
+				It("sholud return the prod endpoint", func() {
+					val := pyxisHostLookup("invalid", "")
+					Expect(val).To(Equal("catalog.redhat.com/api/containers"))
+				})
+			})
+		})
+
+		Context("with a host override", func() {
+			It("should return the override", func() {
+				val := pyxisHostLookup("prod", "overridden")
+				Expect(val).To(Equal("overridden"))
+			})
+		})
+	})
+})
