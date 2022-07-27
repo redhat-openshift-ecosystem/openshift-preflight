244445d894e3a35d8fe5b3c6cb396f5b5a34e457
Sid Kattoju <skattoju@redhat.com>
2022-07-27 16:19:49 -0400

    add check that logs a message if a scc is specified in the csv

---
+++ certification/internal/policy/operator/scc_info.go
@@ -0,0 +1,102 @@
+package operator
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification/internal/bundle"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification"
+)
+
+// securityContextConstraintsInCSV evaluates the csv and logs a message if a non default security context constraint is
+// needed by the operator
+type securityContextConstraintsInCSV struct {
+	customSCCSpecified bool
+}
+
+func NewSecurityContextConstraintsCheck() *securityContextConstraintsInCSV {
+	return &securityContextConstraintsInCSV{
+		false,
+	}
+}
+
+func (p *securityContextConstraintsInCSV) Validate(ctx context.Context, bundleRef certification.ImageReference) (bool, error) {
+	requestedSCCList, err := p.dataToValidate(ctx, bundleRef.ImageFSPath)
+	if err != nil {
+		return false, err
+	}
+
+	return p.validate(ctx, requestedSCCList)
+}
+
+func (p *securityContextConstraintsInCSV) dataToValidate(ctx context.Context, imagePath string) ([]string, error) {
+	csvFilepath, err := bundle.GetCsvFilePathFromBundle(imagePath)
+	if err != nil {
+		return nil, err
+	}
+
+	csvFileReader, err := os.Open(csvFilepath)
+	if err != nil {
+		return nil, err
+	}
+
+	requestedSccList, err := bundle.GetSecurityContextConstraints(ctx, csvFileReader)
+	if err != nil {
+		return nil, fmt.Errorf("unable to extract security context constraints from ClusterServiceVersion: %w", err)
+	}
+
+	return requestedSccList, nil
+}
+
+func (p *securityContextConstraintsInCSV) validate(ctx context.Context, requestedSccList []string) (bool, error) {
+	if len(requestedSccList) == 0 {
+		log.Infof("No custom security context constraint was detected in the CSV. The default restricted SCC will be used.")
+		return true, nil
+	}
+
+	if len(requestedSccList) != 1 {
+		return false, fmt.Errorf("only one scc should be requested at a time")
+	}
+
+	log.Infof("A custom scc was specified: %s , action may be needed by a cluster admin, please check the "+
+		"operator documentation for more information", requestedSccList[0])
+	// set bool to change help text
+	p.customSCCSpecified = true
+	return true, nil
+}
+
+func (p *securityContextConstraintsInCSV) Name() string {
+	return "SecurityContextConstraintsInCSV"
+}
+
+func (p *securityContextConstraintsInCSV) Metadata() certification.Metadata {
+	return certification.Metadata{
+		Description:      "Evaluates the csv and logs a message if a non default security context constraint is needed by the operator",
+		Level:            "optional",
+		KnowledgeBaseURL: "https://redhat-connect.gitbook.io/certified-operator-guide/troubleshooting-and-resources/sccs", // Placeholder
+		CheckURL:         "https://redhat-connect.gitbook.io/certified-operator-guide/troubleshooting-and-resources/sccs", // Placeholder
+	}
+}
+
+func (p *securityContextConstraintsInCSV) Help() certification.HelpText {
+	// TODO implement a way to display help text for informational checks
+	// Currently this bool triggered customized help is not shown anywhere since this check is purely informational
+	// and always passes. Since the ask is to indicate this information to the user it should eventually be shown in the
+	// output rather than just logs
+	if p.customSCCSpecified {
+		return certification.HelpText{
+			Message: "A custom security context constraint was detected in the csv. " +
+				"Please see the operators documentation for details.",
+			Suggestion: "A custom security context constraint may need to be applied by a cluster admin.",
+		}
+	}
+	// This is the default help text returned in case the check fails
+	return certification.HelpText{
+		Message: "The securityContextConstraintsInCSV logs a message if the operator requests a " +
+			"security context constraint. Please review the operators documentation to see if this is needed.",
+		Suggestion: "If no scc is detected the default restricted scc will be used.",
+	}
+}
---
+++ certification/internal/policy/operator/scc_info_test.go
@@ -0,0 +1,106 @@
+package operator
+
+import (
+	"context"
+	"os"
+	"path/filepath"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/redhat-openshift-ecosystem/openshift-preflight/certification"
+)
+
+var _ = Describe("securityContextConstraintsInCSV", func() {
+	const (
+		manifestsDir                  = "manifests"
+		clusterServiceVersionFilename = "myoperator.clusterserviceversion.yaml"
+		customSCC                     = "\n      clusterPermissions:\n      - rules:\n        - apiGroups:\n          - security.openshift.io\n          resources:\n          - securitycontextconstraints\n          resourceNames:\n          - custom\n          verbs:\n          - use\n        serviceAccountName: my-operand\n"
+		multipleSCC                   = "\n      clusterPermissions:\n      - rules:\n        - apiGroups:\n          - security.openshift.io\n          resources:\n          - securitycontextconstraints\n          resourceNames:\n          - anyuid\n          - nonroot\n          verbs:\n          - use\n        serviceAccountName: my-operand\n"
+		noSCC                         = "\n"
+	)
+	var (
+		securityContextConstraintsCheck securityContextConstraintsInCSV
+		imageRef                        certification.ImageReference
+		csvContents                     = buildCsvContent(noSCC)
+	)
+	AssertMetaData(&securityContextConstraintsCheck)
+	BeforeEach(func() {
+		securityContextConstraintsCheck = *NewSecurityContextConstraintsCheck()
+		tmpDir, err := os.MkdirTemp("", "scc-check-bundle-*")
+		Expect(err).ToNot(HaveOccurred())
+		imageRef.ImageFSPath = tmpDir
+		DeferCleanup(os.RemoveAll, tmpDir)
+		err = os.Mkdir(filepath.Join(tmpDir, manifestsDir), 0o755)
+		Expect(err).ToNot(HaveOccurred())
+		err = os.WriteFile(filepath.Join(tmpDir, manifestsDir, clusterServiceVersionFilename), []byte(csvContents), 0o644)
+		Expect(err).ToNot(HaveOccurred())
+	})
+	When("given a csv with a custom scc", func() {
+		It("should succeed", func() {
+			csvContents := buildCsvContent(customSCC)
+			Expect(os.WriteFile(filepath.Join(imageRef.ImageFSPath, manifestsDir, clusterServiceVersionFilename), []byte(csvContents), 0o644)).To(Succeed())
+			result, err := securityContextConstraintsCheck.Validate(context.TODO(), imageRef)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result).To(BeTrue())
+			Expect(securityContextConstraintsCheck.Help().Message).To(ContainSubstring("custom"))
+		})
+	})
+	When("given a csv with a multiples sccs", func() {
+		It("should fail", func() {
+			csvContents := buildCsvContent(multipleSCC)
+			Expect(os.WriteFile(filepath.Join(imageRef.ImageFSPath, manifestsDir, clusterServiceVersionFilename), []byte(csvContents), 0o644)).To(Succeed())
+			result, err := securityContextConstraintsCheck.Validate(context.TODO(), imageRef)
+			Expect(err).To(HaveOccurred())
+			Expect(result).To(BeFalse())
+		})
+	})
+	When("there is no CSV", func() {
+		It("should fail", func() {
+			Expect(os.RemoveAll(imageRef.ImageFSPath)).To(Succeed())
+			result, err := securityContextConstraintsCheck.Validate(context.TODO(), imageRef)
+			Expect(err).To(HaveOccurred())
+			Expect(result).To(BeFalse())
+		})
+	})
+	When("the CSV is malformed", func() {
+		It("should fail", func() {
+			csvContents := `kind: ClusterServiceVersion
+apiVersion: operators.coreos.com/v1alpha1
+spec: malformed
+`
+			Expect(os.WriteFile(filepath.Join(imageRef.ImageFSPath, manifestsDir, clusterServiceVersionFilename), []byte(csvContents), 0o644)).To(Succeed())
+			result, err := securityContextConstraintsCheck.Validate(context.TODO(), imageRef)
+			Expect(err).To(HaveOccurred())
+			Expect(result).To(BeFalse())
+		})
+	})
+	When("no scc is specfied in csv", func() {
+		It("should still pass", func() {
+			csvContents := buildCsvContent(noSCC)
+			Expect(os.WriteFile(filepath.Join(imageRef.ImageFSPath, manifestsDir, clusterServiceVersionFilename), []byte(csvContents), 0o644)).To(Succeed())
+			result, err := securityContextConstraintsCheck.Validate(context.TODO(), imageRef)
+			Expect(err).ToNot(HaveOccurred())
+			Expect(result).To(BeTrue())
+		})
+	})
+})
+
+func buildCsvContent(sccBlock string) string {
+	return `kind: ClusterServiceVersion
+apiVersion: operators.coreos.com/v1alpha1
+spec:
+  install:
+    spec:
+      deployments:
+      - spec:
+          template:
+            spec:
+              containers:
+              - image: registry.example.io/foo/bar@sha256:f000432f07cd187469f0310e3ed9dcf9a5db2be14b8bab9c5293dd1ee8518176
+                name: the-operator` + sccBlock +
+		`relatedImages:
+  - name: the-operator
+    image: registry.example.io/foo/bar@sha256:f000432f07cd187469f0310e3ed9dcf9a5db2be14b8bab9c5293dd1ee8518176
+  - name: the-proxy
+    image: registry.example.io/foo/proxy@sha256:5e33f9d095952866b9743cc8268fb740cce6d93439f00ce333a2de1e5974837e`
+}
