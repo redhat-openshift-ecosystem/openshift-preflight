7983a9740f54d4c9f3b5fb75e617b1e0b4be1bcb
asaswadk <asaswadk@redhat.com>
2023-06-20 19:08:39 +0530

    update cert_document URL

--- internal/policy/container/defaults.go
+++ internal/policy/container/defaults.go
@@ -1,3 +1,3 @@
 package container
 
-var certDocumentationURL = "https://access.redhat.com/documentation/en-us/red_hat_software_certification/8.45/html/red_hat_openshift_software_certification_policy_guide/assembly-requirements-for-container-images_openshift-sw-cert-policy-introduction"
+var certDocumentationURL = "https://access.redhat.com/documentation/en-us/red_hat_software_certification/8.63/html-single/red_hat_openshift_software_certification_policy_guide/index#assembly-requirements-for-container-images_openshift-sw-cert-policy-introduction"
