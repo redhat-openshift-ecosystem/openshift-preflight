783cb570ad20c2490e54d2e12a73277732f20b60
Adam D. Cornett <adc@redhat.com>
2023-06-21 07:38:44 -0700

    removing unused bundle validate code that relied on operator-sdk binary
    Signed-off-by: Adam D. Cornett <adc@redhat.com>

--- internal/policy/operator/types.go
+++ internal/policy/operator/types.go
@@ -7,6 +7,5 @@ import (
 )
 
 type operatorSdk interface {
-	BundleValidate(context.Context, string, operatorsdk.OperatorSdkBundleValidateOptions) (*operatorsdk.OperatorSdkBundleValidateReport, error)
 	Scorecard(context.Context, string, operatorsdk.OperatorSdkScorecardOptions) (*operatorsdk.OperatorSdkScorecardReport, error)
 }
