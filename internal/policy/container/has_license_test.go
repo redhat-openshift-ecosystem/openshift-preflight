83813644338ab692c39ba780787aecc244c16a92
Caleb Xu <caxu@redhat.com>
2026-03-26 13:19:45 -0400

    HasLicense: search subdirs and don't count dirs
    Only consider files, not directories, as valid license files, and
    search in subdirs within the license directory, as valid license
    files may not be at the top level.
    
    Signed-off-by: Caleb Xu <caxu@redhat.com>

--- internal/policy/container/has_license_test.go
+++ internal/policy/container/has_license_test.go
@@ -17,63 +17,109 @@ const (
 	licenses     = "licenses"
 )
 
+func setupTmpDir() string {
+	tmpDir, err := os.MkdirTemp("", "license-check-*")
+	Expect(err).ToNot(HaveOccurred())
+	DeferCleanup(func() {
+		os.RemoveAll(tmpDir)
+	})
+	return tmpDir
+}
+
+func createLicenseDir(tmpDir string) {
+	err := os.Mkdir(filepath.Join(tmpDir, licenses), 0o755)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func createLicenseFile(tmpDir, path string) {
+	fullPath := filepath.Join(tmpDir, licenses, path)
+	err := os.MkdirAll(filepath.Dir(fullPath), 0o755)
+	Expect(err).ToNot(HaveOccurred())
+	err = os.WriteFile(fullPath, []byte("This is a license"), 0o644)
+	Expect(err).ToNot(HaveOccurred())
+}
+
+func createEmptyFile(tmpDir, path string) {
+	fullPath := filepath.Join(tmpDir, licenses, path)
+	_, err := os.Create(fullPath)
+	Expect(err).ToNot(HaveOccurred())
+}
+
 var _ = Describe("HasLicense", func() {
 	var hasLicense HasLicenseCheck
 
 	Describe("Checking if licenses can be found", func() {
-		var imgRef image.ImageReference
-		BeforeEach(func() {
-			var err error
-			tmpDir, err := os.MkdirTemp("", "license-check-*")
-			Expect(err).ToNot(HaveOccurred())
-			err = os.Mkdir(filepath.Join(tmpDir, licenses), 0o755)
-			Expect(err).ToNot(HaveOccurred())
-			err = os.WriteFile(filepath.Join(tmpDir, licenses, validLicense), []byte("This is a license"), 0o644)
-			Expect(err).ToNot(HaveOccurred())
-			_, err = os.Create(filepath.Join(tmpDir, licenses, emptyLicense))
-			Expect(err).ToNot(HaveOccurred())
-			imgRef.ImageFSPath = tmpDir
-		})
-		Context("When license(s) are found", func() {
+		Context("When license(s) are found at top level", func() {
 			It("Should pass Validate", func() {
-				ok, err := hasLicense.Validate(context.TODO(), imgRef)
+				tmpDir := setupTmpDir()
+				createLicenseDir(tmpDir)
+				createLicenseFile(tmpDir, validLicense)
+				createEmptyFile(tmpDir, emptyLicense)
+
+				ok, err := hasLicense.Validate(context.TODO(), image.ImageReference{ImageFSPath: tmpDir})
 				Expect(err).ToNot(HaveOccurred())
 				Expect(ok).To(BeTrue())
 			})
 		})
+
 		Context("When licenses directory is not found", func() {
 			It("Should not pass Validate", func() {
-				badImgRef := imgRef
-				badImgRef.ImageFSPath = "/invalid"
-				ok, err := hasLicense.Validate(context.TODO(), badImgRef)
+				ok, err := hasLicense.Validate(context.TODO(), image.ImageReference{ImageFSPath: "/invalid"})
 				Expect(err).ToNot(HaveOccurred())
 				Expect(ok).To(BeFalse())
 			})
 		})
-		Context("Licenses can't be found when directory exists", func() {
-			JustBeforeEach(func() {
-				os.Remove(filepath.Join(imgRef.ImageFSPath, licenses, validLicense))
-				os.Remove(filepath.Join(imgRef.ImageFSPath, licenses, emptyLicense))
-			})
+
+		// This shouldn't happen in practice, since the untar extraction
+		// logic will prune/not create empty directories.
+		Context("When licenses directory exists but is empty", func() {
 			It("Should not pass Validate", func() {
-				ok, err := hasLicense.Validate(context.TODO(), imgRef)
+				tmpDir := setupTmpDir()
+				createLicenseDir(tmpDir)
+
+				ok, err := hasLicense.Validate(context.TODO(), image.ImageReference{ImageFSPath: tmpDir})
 				Expect(err).ToNot(HaveOccurred())
 				Expect(ok).To(BeFalse())
 			})
 		})
-		Context("Only an empty license", func() {
-			JustBeforeEach(func() {
-				os.Remove(filepath.Join(imgRef.ImageFSPath, licenses, validLicense))
-			})
+
+		Context("When only an empty license file exists", func() {
 			It("Should not pass Validate", func() {
-				ok, err := hasLicense.Validate(context.TODO(), imgRef)
+				tmpDir := setupTmpDir()
+				createLicenseDir(tmpDir)
+				createEmptyFile(tmpDir, emptyLicense)
+
+				ok, err := hasLicense.Validate(context.TODO(), image.ImageReference{ImageFSPath: tmpDir})
 				Expect(err).ToNot(HaveOccurred())
 				Expect(ok).To(BeFalse())
 			})
 		})
-		AfterEach(func() {
-			err := os.RemoveAll(imgRef.ImageFSPath)
-			Expect(err).ToNot(HaveOccurred())
+
+		// This shouldn't happen in practice, since the untar extraction
+		// logic will prune/not create empty directories.
+		Context("When only directories exist in the license folder", func() {
+			It("Should not pass Validate", func() {
+				tmpDir := setupTmpDir()
+				createLicenseDir(tmpDir)
+				err := os.MkdirAll(filepath.Join(tmpDir, licenses, "just-another-dir"), 0o755)
+				Expect(err).ToNot(HaveOccurred())
+
+				ok, err := hasLicense.Validate(context.TODO(), image.ImageReference{ImageFSPath: tmpDir})
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ok).To(BeFalse())
+			})
+		})
+
+		Context("When license is found only in nested subdirectory", func() {
+			It("Should pass Validate", func() {
+				tmpDir := setupTmpDir()
+				createLicenseDir(tmpDir)
+				createLicenseFile(tmpDir, filepath.Join("a/b/c", validLicense))
+
+				ok, err := hasLicense.Validate(context.TODO(), image.ImageReference{ImageFSPath: tmpDir})
+				Expect(err).ToNot(HaveOccurred())
+				Expect(ok).To(BeTrue())
+			})
 		})
 
 		AssertMetaData(&hasLicense)
