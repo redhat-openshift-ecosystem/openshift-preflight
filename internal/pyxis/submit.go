1c6db6008e74e8b17d51357822f22c98382bb279
Brad P. Crochet <bcrochet@redhat.com>
2026-04-14 12:31:36 -0400

    pyxis: remove coverage exclusion from artifact ImageID assignment in submit.go
    Refs: #1420

--- internal/pyxis/submit.go
+++ internal/pyxis/submit.go
@@ -109,7 +109,6 @@ func (p *pyxisClient) SubmitResults(ctx context.Context, certInput *Certificatio
 	// Create the artifacts in Pyxis.
 	artifacts := certInput.Artifacts
 	for _, artifact := range artifacts {
-		//coverage:ignore
 		artifact.ImageID = certImage.ID
 		if _, err := p.createArtifact(ctx, &artifact); err != nil {
 			//coverage:ignore
